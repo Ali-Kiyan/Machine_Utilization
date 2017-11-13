8ca969932b80dee0ce6a44c458603141546d759b
Ali <alikiyand@gmail.com>
2017-11-13 17:21:35 +0000

    adding deliverable of the the coal miner

---
+++ MU.R
@@ -0,0 +1,7 @@
+#Deliverable - a list with the following components:
+#Character: machine name 
+#Vector: (min, mean, max) Utilization for the month (excluding unknown hours)
+#Logical: Has utilization ever fallen below 90% ? TRUE / FALSE
+#Vector: All hours where utilization is unknown (NA's)
+#Dataframe: for this machine
+#Plot For all machines 
