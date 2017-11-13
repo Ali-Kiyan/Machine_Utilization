6c7a7c69f75d525c3939b27e54bde38dd81358b5
Ali <alikiyand@gmail.com>
2017-11-13 20:17:46 +0000

    working with time posx and rearenging the dataset columns

--- MU.R
+++ MU.R
@@ -5,3 +5,18 @@
 #Vector: All hours where utilization is unknown (NA's)
 #Dataframe: for this machine
 #Plot For all machines 
+util <- read.csv("Machine-Utilization.csv")
+head(util, 12)
+str(util)
+summary(util)
+#Derive utilization
+util$Utilization <- 1-util$Percent.Idle
+head(util, 12)
+#POSIX is a standard for everything when you are moving from one machine to the other so basically handles time in R
+util$PosixTime <- as.POSIXct(util$Timestamp, format="%d/%m/%Y %H:%M") 
+head(util,12)
+summary(util)
+#tip how to rearenge cols in a df
+util$Timestamp <- NULL
+head(util,12)
+util <- util[,c(4,1,2,3)]
